d3e317e60e4237408ed3a3eba04d99e8268452ac
annika <annikaslund@gmail.com>
2019-03-18 15:47:00 -0700

    Add remove() to helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,17 @@
 
 //returns a randomly selected item from array of items
-function choice(item) {
-    return item[Math.floor(Math.random() * item.length)];
+function choice(items) {
+    return items[Math.floor(Math.random() * items.length)];
 }
 
-export { choice };
+// returns first matching item from items. if item does not exist, return undefined.
+function remove(items, item){
+    let index = items.indexOf(item);
+    if (index === -1){
+        return undefined;
+    };
+
+    return items.splice(index,1);
+}
+
+export { choice, remove };
