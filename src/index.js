6291bcd7be5cc74b744d7b131c6b626b6f47ac3d
Chengsi <gaochengsi@gmail.com>
2019-03-18 15:54:57 -0700

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import fruits from "./foods";
+import { choice, remove } from "./helpers";
+
+//randomly draw a fruit from the array
+let randomFruit = choice(fruits);
+console.log(`I'd like one ${randomFruit}, please.`);
